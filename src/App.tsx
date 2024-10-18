b509df5daf0abf7f0b178d680d11b0a063e944d1
Euaell <euaelmeko@gmail.com>
2024-10-18 23:39:24 +0300

    card done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css'
-import { Badge, Banner, Toast } from './components'
+import { Badge, Banner, Card, Toast } from './components'
+import testSvg from './test.svg'
 
 function App() {
 	
 	return (
 		<>
+		
+			{/* <BannerAndBadge /> */}
+
+			<CardDisplay />
+		</>
+	)
+}
+
+function BannerAndBadge() {
+	return (
+		<>
 			<Toast>Hello Toast!!</Toast>
-			<Badge color='red'>Badge</Badge>
+			<Badge color='red' className='cursor-pointer' onClick={() => console.log("badge clicked")}>Badge</Badge>
 			<Badge color='yellow'>Badge</Badge>
 			<Badge color='green'>Badge</Badge>
 			<Badge>Badge</Badge>
@@ -17,6 +29,11 @@ function App() {
 
 			<Banner title='Info' status='info'>This is an info banner</Banner>
 			<Banner title='Error' status='error'>This is an error banner</Banner>
+			<Banner title='with no content' status='error' />
+			<Banner title='with no content' status='warning' />
+			<Banner title='with no content' status='success' />
+			<Banner title='with no content' />
+
 			<Banner title='Success' status='success' className='inline-block'>This is a success banner</Banner>
 			<Banner title='Warning' status='warning' className='inline-block'>This is a warning banner</Banner>
 			{/* A banner with a longer text */}
@@ -25,6 +42,23 @@ function App() {
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam quam placeat esse, ea accusantium quaerat necessitatibus impedit ipsam facere cumque incidunt ad, cupiditate ab modi est laboriosam quae sit nemo.
 			</Banner>
 		</>
+	)
+}
+
+function CardDisplay() {
+	return (
+		<div className='bg-gray-200'>
+			
+			<Card title='First Title' icon={<img src={testSvg} alt='test svg' />}>
+				<p>This is a card</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ad quidem labore architecto maiores possimus, porro cum placeat eligendi eius necessitatibus doloremque iure minima perferendis minus alias. Suscipit, aspernatur atque?</p>
+			</Card>
+
+			<Card title='Awesome Title' icon={<img src={testSvg} alt='test svg' />}>
+				<p>This is a card</p>
+				<p>Lorem iplaceat eligendi eius necessitatibus doloremque iure minima perferendis minus alias. </p>
+			</Card>
+		</div>
 	)
 }
 
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,15 +1,38 @@
-import { twMerge } from "tailwind-merge";
+import { twJoin, twMerge } from "tailwind-merge";
 
 interface CardProps {
-    children: React.ReactNode;
-    className?: string;
-    [x: string]: any;
+	children: Readonly<React.ReactNode>;
+	title: Readonly<string>;
+	icon?: Readonly<React.ReactNode>;
+	// iconPosition?: 'top' | 'bottom' | 'left' | 'right';
+	iconBgColor?: string;
+	className?: string;
+	[x: string]: any;
 }
 
-export default function Card({ children, className, ...rest }: CardProps) {
-    return (
-        <div className={twMerge('inline-flex', className)} {...rest}>
-            {children}
-        </div>
-    )
+export default function Card({ children, className, title, icon, iconBgColor = 'bg-[#3F75FE]', ...rest }: CardProps) {
+	return (
+		<div className={twMerge(
+				'relative inline-flex flex-col p-8 m-7 bg-white min-w-56 max-w-96 rounded-lg shadow-sm transition-shadow duration-200 hover:shadow-xl',
+				className
+			)} 
+			{...rest}
+		>
+			{/* An absolutely positioned rectangular container for the icon */}
+			{icon && 
+				<div className="absolute flex items-center justify-center left-0 top-0 w-full h-fit">
+					<div className={twJoin('relative p-2 w-12 h-12 flex items-center justify-center rounded-lg -top-6 shadow-xl', iconBgColor)}>
+						{icon}
+					</div>
+				</div>
+			}
+
+			<div className='flex items-center py-5'>
+				<h1 className='text-xl font-bold text-[#111827] w-full text-center'>{title}</h1>
+			</div>
+			<div className="text-[#6B7280] text-center">
+				{children}
+			</div>
+		</div>
+	)
 }
